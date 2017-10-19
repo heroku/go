8ea2734d023b87b69edd6e84566e509e36b93b1a
Ben Burkert <ben@benburkert.com>
2017-10-19 10:29:23 -0700

    net/http: ignore response body when forbidden by status code
    A 1XX, 204, or 304 response may not include a response body according
    to RFC 7230, section 3.3.3. If a buggy server returns a 204 or 304
    response with a body that is chunked encoded, the invalid body is
    currently made readable in the Response. This can lead to data races due
    to the transport connection's read loop which does not wait for the body
    EOF when the response status is 204 or 304.
    
    The correct behavior is to ignore the body on a 204 or 304 response, and
    treat the body data as the beginning of the next request on the
    connection.
    
    Updates #22330.
    
    Change-Id: I89a457ceb783b6f66136d5bf9be0a9b0a04fa955
    Reviewed-on: https://go-review.googlesource.com/71910
    Reviewed-by: Tom Bergan <tombergan@google.com>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Run-TryBot: Tom Bergan <tombergan@google.com>

--- src/net/http/transport_test.go
+++ src/net/http/transport_test.go
@@ -4118,3 +4118,25 @@ var rgz = []byte{
 	0x00, 0x00, 0x3d, 0xb1, 0x20, 0x85, 0xfa, 0x00,
 	0x00, 0x00,
 }
+
+// Issue 22330: do not allow the response body to be read when the status code
+// forbids a response body.
+func TestNoBodyOnChunked304Response(t *testing.T) {
+	defer afterTest(t)
+	cst := newClientServerTest(t, h1Mode, HandlerFunc(func(w ResponseWriter, r *Request) {
+		conn, buf, _ := w.(Hijacker).Hijack()
+		buf.Write([]byte("HTTP/1.1 304 NOT MODIFIED\r\nTransfer-Encoding: chunked\r\n\r\n0\r\n\r\n"))
+		buf.Flush()
+		conn.Close()
+	}))
+	defer cst.close()
+
+	res, err := cst.c.Get(cst.ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.Body != NoBody {
+		t.Errorf("Unexpected body on 304 response")
+	}
+}
